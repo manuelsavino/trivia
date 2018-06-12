24d90e3d2a93afd426c5b65c9bca1119af1b2d81
Manuel Savino <manuelsavino@gmail.com>
2018-06-12 08:51:38 -0400

    randomize order of answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,46 +1,59 @@
-$(function (){
-var game = {
-    questions: ["What colos is the sky?", "What color is the grass?", "How many fingers do I have?"],
-    wrongAnswers: ["green, red, yellow", "blue,red,purple", "1,5,8"],
-    rightAnswers: ["blue", "green", "10"],
-    rightAnswerCount: 0,
-    wronganswerCount: 0,
-}
-
-var question = game.questions.length -1
-var timer = 1;
-var intervalVal = setInterval(Timer, 1000);
-
-function Timer()
-{
-    console.log(--timer)
-    if(timer === 0)
-    {   
-        if (question === -1) {
-            clearInterval(intervalVal)
-            //call results function
-        }
-        else{
-            showQuestion();
-            timer = 5
-            question--
-        }
-       
+$(function () {
+    var game = {
+        questions: ["What colos is the sky?", "What color is the grass?", "How many fingers do I have?"],
+        wrongAnswers: ["green, red, yellow", "blue,red,purple", "1,5,8"],
+        rightAnswers: ["blue", "green", "10"],
+        rightAnswerCount: 0,
+        wronganswerCount: 0,
     }
-}
 
-function showQuestion(){
-    var currentQuestion = game.questions[question]
-    $("#questions").text(currentQuestion)
-    var correctAnswer = game.rightAnswers[question]
-    var options = game.wrongAnswers[question].split(",")
+    var question = game.questions.length - 1
+    var timer = 1;
+    var intervalVal = setInterval(StartGame, 1000);
+
+    function StartGame() {
+        console.log(--timer)
+        if (timer === 0) {
+            if (question === -1) {
+                clearInterval(intervalVal)
+                //call results function
+            }
+            else {
+                showQuestion();
+                question--
+            }
+        }
+    }
+    function showQuestion() {
+        timer = 5
+        //fill in question 
+        var currentQuestion = game.questions[question]
+        $("#questions").text(currentQuestion)
+
+
+        var correctAnswer = "<button class='btn btn-primary d-block' correct='yes'>" + game.rightAnswers[question] + "</button>"
+        var options = game.wrongAnswers[question].split(",")
+        var allOptions = [correctAnswer];
+        options.forEach(function (element) {
+            allOptions.push("<button class='btn btn-primary d-block' correct='no'>" + element + "</button>")
+        })
+
+        //Shuffle the order of the answers
+        for (var i = allOptions.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = allOptions[i];
+            allOptions[i] = allOptions[j];
+            allOptions[j] = temp;
+        }
+        //Clear answers and fil in new set
         $("#answers").empty()
-        for(i=0; i< options.length; i++)
-        {
-            $("#answers").append("<button class='btn btn-primary'>"+ options[i]+"</button")
+        for (i = 0; i < allOptions.length; i++) {
+            $("#answers").append(allOptions[i])
         }
 
-    console.log("Question "+(question + 1) +": "+currentQuestion + " Wrong Answers: " + options + "\nRight Answer: " + correctAnswer)
-}
+
+    }
+
+
 
 })
