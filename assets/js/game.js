b9c249775fe12b7fd63ebc9c2ec3f751b099c2da
Manuel Savino <msavino@fiu.edu>
2018-06-12 16:47:13 -0400

    recreated game1.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,59 +1,53 @@
 $(function () {
     var game = {
-        questions: ["What colos is the sky?", "What color is the grass?", "How many fingers do I have?"],
-        wrongAnswers: ["green, red, yellow", "blue,red,purple", "1,5,8"],
-        rightAnswers: ["blue", "green", "10"],
-        rightAnswerCount: 0,
-        wronganswerCount: 0,
+        questions: ["What colos is the sky?", "What planet do we leave in?", "How many fingers do I have?"],
+        wrongAnswers: ["green,red,purple", "Jupiter,Mars,Saturn", "17,12,6"],
+        rightAnswers: ["Blue", "Earth", "10"]
     }
+    var clockRunning = false
 
-    var question = game.questions.length - 1
-    var timer = 1;
-    var intervalVal = setInterval(StartGame, 1000);
+var time = 10;
+    $("#newGame").on("click", start)
 
-    function StartGame() {
-        console.log(--timer)
-        if (timer === 0) {
-            if (question === -1) {
-                clearInterval(intervalVal)
-                //call results function
-            }
-            else {
-                showQuestion();
-                question--
-            }
+    function start() {
+        if (!clockRunning) {
+            intervalVar = setInterval(decreaseTime, 1000)
+            console.log(time)
+            clockRunning = true;
+            
         }
     }
+
+    function decreaseTime()
+    {
+        time--
+    }
+
+
+
     function showQuestion() {
-        timer = 5
         //fill in question 
-        var currentQuestion = game.questions[question]
-        $("#questions").text(currentQuestion)
+            var currentQuestion = game.questions[question]
+            $("#questions").text(currentQuestion)
+            var correctAnswer = "<button class='btn btn-primary d-block choice' correct='yes'>" + game.rightAnswers[question] + "</button>"
+            var options = game.wrongAnswers[question].split(",")
+            var allOptions = [correctAnswer];
+            options.forEach(function (element) {
+                allOptions.push("<button class='btn btn-primary d-block choice' correct='no'>" + element + "</button>")
+            })
 
-
-        var correctAnswer = "<button class='btn btn-primary d-block' correct='yes'>" + game.rightAnswers[question] + "</button>"
-        var options = game.wrongAnswers[question].split(",")
-        var allOptions = [correctAnswer];
-        options.forEach(function (element) {
-            allOptions.push("<button class='btn btn-primary d-block' correct='no'>" + element + "</button>")
-        })
-
-        //Shuffle the order of the answers
-        for (var i = allOptions.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = allOptions[i];
-            allOptions[i] = allOptions[j];
-            allOptions[j] = temp;
-        }
-        //Clear answers and fil in new set
-        $("#answers").empty()
-        for (i = 0; i < allOptions.length; i++) {
-            $("#answers").append(allOptions[i])
-        }
-
-
+            //Shuffle the order of the answers
+            for (var i = allOptions.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = allOptions[i];
+                allOptions[i] = allOptions[j];
+                allOptions[j] = temp;
+            }
+            //Clear answers and fil in new set for new quetion
+            $("#answers").empty()
+            for (i = 0; i < allOptions.length; i++) {
+                $("#answers").append(allOptions[i])
+            }
+        
     }
-
-
-
 })
---
+++ assets/js/game1.js
@@ -0,0 +1,102 @@
+$(function () {
+    var game = {
+        questions: ["What colos is the sky?", "What planet do we leave in?", "How many fingers do I have?","What do you write with?"],
+        wrongAnswers: ["green,red,purple", "Jupiter,Mars,Saturn", "17,12,6","paper,mouse,other Wrong"],
+        rightAnswers: ["Blue", "Earth", "10","Pen"]
+    }
+
+    var time = 5
+    var running = false
+    var intervalVal;
+    var currentQuestion = game.questions.length - 1;
+
+    $("#newGame").on("click", start)
+    function start() {
+        if (!running) {
+            intervalVal = setInterval(decereaseTime, 1000)
+            $("#timer").text("10")
+            showQuestion(currentQuestion)
+            running = true;
+        }
+    }
+
+    function decereaseTime() {
+        time--
+        if (time === 0) {
+            clearInterval(intervalVal);
+            running = false;
+            //change to show correct or wrong
+            if (currentQuestion >= 1) {
+                clearQuestion()
+                $("#questions").text(game.rightAnswers[currentQuestion])
+                setTimeout(start, 2000)
+                time = 5
+                currentQuestion--
+            }
+            else {
+                //game over
+                console.log("gameOver")
+                gameOver()
+                clearQuestion()
+            }
+        }
+        else if (time >= 1) {
+            //checking for click goes here
+            console.log("playing")
+            $(".choice").on("click", function(){
+                
+                var ans = $(this).attr("answer")
+                
+                if (ans === game.rightAnswers[currentQuestion])
+                {
+                    $("#answers").append("yes you're right")
+                }
+                
+            })
+            
+            
+
+        }
+        // console.log(time)
+        $("#timer").text(time)
+    }
+
+
+    function showQuestion(val) {
+        //fill in question 
+        var currentQuestion = game.questions[val]
+        $("#questions").text(currentQuestion)
+
+        //ansers
+        var correctAnswer = "<button class='btn btn-primary d-block choice' answer='" + game.rightAnswers[val] + "'>" + game.rightAnswers[val] + "</button>"
+        var options = game.wrongAnswers[val].split(",")
+        var allOptions = [correctAnswer];
+        options.forEach(function (element) {
+            allOptions.push("<button class='btn btn-primary d-block choice' answer='" + game.rightAnswers[val] + "'>" + element + "</button>")
+        })
+
+        //Shuffle the order of the answers
+        for (var i = allOptions.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = allOptions[i];
+            allOptions[i] = allOptions[j];
+            allOptions[j] = temp;
+        }
+        //Clear answers and fil in new set for new quetion
+        $("#answers").empty()
+        for (i = 0; i < allOptions.length; i++) {
+            $("#answers").append(allOptions[i])
+        }
+    }
+    function clearQuestion() {
+        $("#answers").empty();
+        $("#questions").empty();
+        $("#timer").text("")
+    }
+
+    function gameOver()
+    {
+        time = 5
+        currentQuestion = game.questions.length - 1;
+    }
+})
